a8ca8e919ed3eeb7ab3528736e644a886d6a9685
Hrishi34 <83940584+Hrishi34@users.noreply.github.com>
2023-02-15 11:04:03 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World from PES1UG20CS647\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run\n";
+  cout << "HRISHIKESH has successfully built and run\n";
   return 0;
 }
